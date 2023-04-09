3430ccba3dc6abe5d6905b58bbfd52dfcbfea362
NiloufarH <hesari.niloofar@gmail.com>
2023-04-09 23:36:33 +0300

    initial commit

---
+++ SpaceChar.py
@@ -0,0 +1,21 @@
+SplitString = "This string has been\nsplit over\nlines"
+print(SplitString)
+
+tabbedString = "1\t2\t3\t4\t5"
+print(tabbedString)
+
+
+print('The pet shop owner said "No, no, \'e\'s uh,...he\'s resting".')
+print('The pet \shop owner is \sick')
+print(' The pet shop owner said \"hi"')
+print("""The pet shop owner said \"hi" . """)
+
+print(''' The pet shop
+      owner 
+      is
+      sick''')
+
+
+
+print('C:\\Users\\timbuchalka\\notes.txt')
+print(r'C:\Users\timbuchalka\notes.txt')
